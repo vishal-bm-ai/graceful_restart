7a7775c97c5d2d4335be3d43738e93bd823b4839
Vishal <vishal@brightmoney.co>
2021-04-08 15:00:24 +0530

    indent

--- test_app/views.py
+++ test_app/views.py
@@ -5,7 +5,8 @@ import time
 # Create your views here.
 
 def test(request):
-    for i in range(60):
-    	Count.objects.create(cnt=4)
-	time.sleep(1)
+    for i in range(120):
+        print(i)
+        Count.objects.create(cnt=4)
+	    time.sleep(1)
     return HttpResponse(f'Hello')
